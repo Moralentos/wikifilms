56bb14967799ef482cfe1babf55a11b21831a58d
Moralentos <salavat_swat@mail.ru>
2023-10-25 18:02:02 +0300

    null

--- src/Redux/seriesPageSlice.js
+++ src/Redux/seriesPageSlice.js
@@ -6,7 +6,7 @@ const apiKey = '0a0ca537-d80e-4b20-b65e-08a5c1baeb19';
 export const getSeries = () => async (dispatch) => {
   try {
     const response = await axios.get(
-      'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1',
+      'https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_250_TV_SHOWS&page=1',
       {
         headers: {
           'X-API-KEY': apiKey,
--- src/pages/SeriesPage/Series.jsx
+++ src/pages/SeriesPage/Series.jsx
@@ -1,40 +1,38 @@
-import React from 'react';
-import styles from './series.module.scss';
-import { useSelector, useDispatch } from 'react-redux';
-import { getSeries } from '../../Redux/seriesPageSlice/';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import styles from "./series.module.scss";
+import { useSelector, useDispatch } from "react-redux";
+import { getSeries } from "../../Redux/seriesPageSlice/";
+import { Link } from "react-router-dom";
 const Series = () => {
-  const seriesData = useSelector((state) => state.seriesSlice.series.films);
+  const seriesData = useSelector((state) => state.seriesSlice.series.items);
   const dispatch = useDispatch();
+
   React.useEffect(() => {
-    // fetchData();
     dispatch(getSeries());
-    console.log(seriesData);
-  }, []);
+  }, [dispatch]);
 
   return (
     <div className={`container`}>
       <div className={styles.wrapper}>
         <div className={styles.card_block}>
-          {seriesData &&
-            seriesData.map((item, index) => {
-              return (
-                <Link key={index} to={`/film/${item.filmId}`}>
-                  <div className={styles.card}>
-                    <div className={styles.img}>
-                      <img src={item.posterUrlPreview} alt='' />
-                      <div className={styles.card_text}>
-                        <span>{`${item.year}, ${item.countries[0].country}, ${item.genres[0].genre}`}</span>
-
-                        <span>{`Длительность: ${item.filmLength}`} </span>
-                      </div>
+          {seriesData && seriesData.length > 0 ? (
+            seriesData.map((item, index) => (
+              <Link key={index} to={`/film/${item.kinopoiskId}`}>
+                <div className={styles.card}>
+                  <div className={styles.img}>
+                    <img src={item.posterUrlPreview} alt="" />
+                    <div className={styles.card_text}>
+                      <span>{`${item.year}, ${item.countries[0].country}, ${item.genres[0].genre}`}</span>
+                      <span>{`Длительность: ${item.filmLength}`} </span>
                     </div>
-                    <span>{item.nameRu}</span>
                   </div>
-                </Link>
-              );
-            })}
+                  <span>{item.nameRu}</span>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
         </div>
       </div>
     </div>
